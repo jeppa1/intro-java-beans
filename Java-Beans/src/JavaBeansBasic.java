90c37ed958ce2529672df9a9ce9f77f5f83e5184
jeppa1 <jadson.jeppa@gmail.com>
2024-05-17 02:03:10 -0300

    first commit

--- Java-Beans/src/JavaBeansBasic.java
+++ Java-Beans/src/JavaBeansBasic.java
@@ -1,5 +1,18 @@
 public class JavaBeansBasic {
     public static void main(String[] args) throws Exception {
         System.out.println("Hello, World!");
+        double salarioMedio=1500.33;
+        String email="aluno@escola.com";
+        String [] emails=["aluno1@escola.com", "aluno2@escola.com"];
+        String meuNome="jeppa";
+        somar(int n1, int n2){};
+        abrirConexao(){};
+        concluirProcessamento(){};
+        findById(int id){};
+        calcularImprimir(){} //há algo de errado, ele deveria ter apenas uma finalidade; 
+    
     }
-}
+
+    
+    }
+
